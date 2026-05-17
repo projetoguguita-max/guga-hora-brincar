506f7cb9df45eec2caf517b1dddbc9398eb60491
rnabais-hue <rnabais@gmail.com>
2026-05-17 18:10:24 -0300

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'guga-hora-brincar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './hora_brincar.html',
+  './calculadora-vamos-tirar-v2.html',
+  './calculadora-vamos-multiplicar.html',
+  './capa-guga.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
