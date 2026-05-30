ae80a0292dbd4b730dc6d5439428dca79ddcbf4b
rnabais-hue <rnabais@gmail.com>
2026-05-30 11:51:55 -0300

    Prefer network for HTML pages

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guga-hora-brincar-v7';
+const CACHE_NAME = 'guga-hora-brincar-v8';
 const ASSETS = [
   './',
   './index.html',
@@ -30,6 +30,20 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  const wantsHtml = event.request.mode === 'navigate' || requestUrl.pathname.endsWith('.html');
+
+  if (wantsHtml) {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match(event.request))
+    );
+    return;
+  }
 
   event.respondWith(
     caches.match(event.request).then(cached => {
